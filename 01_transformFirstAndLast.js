c2455f4cea8af884bb9ba2ab2db24aee912ddfda
2realzoo <lunalee00@gmail.com>
2022-12-01 19:16:18 +0900

    01_transformFirstAndLast

---
+++ 01_transformFirstAndLast.js
@@ -0,0 +1,7 @@
+function transformFirstAndLast(arr) {
+    // TODO: 여기에 코드를 작성합니다.
+    let obj = {};
+    if(arr.length === 0) return {};
+    obj[arr[0]] = arr[arr.length - 1];
+    return obj
+  }
